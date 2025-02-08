7db1df8dae43e944ed4cfa0e667d70c27b6b11b7
imperium.foxes1@gmail.com <imperium.foxes1@gmail.com>
2025-02-08 21:02:26 +0300

    Edit Craft Literium set

--- src/main/java/net/mcreator/ancientruins/item/AncientRemainsItem.java
+++ src/main/java/net/mcreator/ancientruins/item/AncientRemainsItem.java
@@ -1,7 +1,9 @@
 
 package net.mcreator.ancientruins.item;
 
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 
 import net.mcreator.ancientruins.init.AncientRuinsModTabs;
@@ -9,5 +11,20 @@ import net.mcreator.ancientruins.init.AncientRuinsModTabs;
 public class AncientRemainsItem extends Item {
 	public AncientRemainsItem() {
 		super(new Item.Properties().tab(AncientRuinsModTabs.TAB_DRIEVNIIE_RUINY).stacksTo(64).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem() {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		return new ItemStack(this);
+	}
+
+	@Override
+	public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+		return 16F;
 	}
 }
