14c2029dcd3fb3cb0c6036a9a37bb60742e190d9
imperium.foxes1@gmail.com <imperium.foxes1@gmail.com>
2025-02-08 23:16:04 +0300

    Fix Spawn Structure, Fix PickAxe, Fix block ore

--- src/main/java/net/mcreator/ancientruins/block/LiteruimOreBlock.java
+++ src/main/java/net/mcreator/ancientruins/block/LiteruimOreBlock.java
@@ -14,6 +14,8 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.core.BlockPos;
 
+import net.mcreator.ancientruins.init.AncientRuinsModItems;
+
 import java.util.List;
 import java.util.Collections;
 
@@ -42,6 +44,6 @@ public class LiteruimOreBlock extends Block {
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
-		return Collections.singletonList(new ItemStack(this, 1));
+		return Collections.singletonList(new ItemStack(AncientRuinsModItems.LITERIUM_ORE.get()));
 	}
 }
